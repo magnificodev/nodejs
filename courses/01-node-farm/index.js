89fe19371a08c14cf996b545913f84e9edc2b714
magnificodev <magnificodev8@gmail.com>
2024-08-09 21:56:09 +0700

    Creating a simple Web Server

--- courses/01-node-farm/index.js
+++ courses/01-node-farm/index.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const http = require("http");
+
+////////////////////////////////
+// FILES
+///////////////////////////////
 
 // // Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -8,22 +13,36 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output.txt", textOut);
 // console.log("File written!")
 
-// Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-        console.log(data2);
-        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-            console.log(data3);
+// // Non-blocking, asynchronous way
+// fs.readFile("./txt/starttt.txt", "utf-8", (err, data1) => {
+//     if (err) return console.error(`ERROR: ${err.message}`);
 
-            fs.writeFile(
-                "./txt/final.txt",
-                `${data2}\n${data3}`,
-                "utf-8",
-                (err) => {
-                    if (!err) console.log("Your file has been written!");
-                }
-            );
-        });
-    });
+//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//         console.log(data2);
+//         fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile(
+//                 "./txt/final.txt",
+//                 `${data2}\n${data3}`,
+//                 "utf-8",
+//                 (err) => {
+//                     if (err) return console.error(`ERROR: ${err.message}`);
+//                     console.log("Your file has been written!");
+//                 }
+//             );
+//         });
+//     });
+// });
+// console.log("Will read file!");
+
+////////////////////////////////
+// SERVER
+///////////////////////////////
+const server = http.createServer((req, res) => {
+    res.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening to requests on port 8000")
+})
