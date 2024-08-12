8faeebe132e3a08b9f849f860b033f5fe9b80e0a
magnificodev <magnificodev8@gmail.com>
2024-08-12 20:49:18 +0700

    Parsing Variables from URLs

--- courses/01-node-farm/index.js
+++ courses/01-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ////////////////////////////////
 // FILES
@@ -73,10 +74,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
     // Overview page
-    if (pathName === "/" || pathName === "/overview") {
+    if (pathname === "/" || pathname === "/overview") {
         res.writeHead(200, { "Content-Type": "text/html" });
 
         const cardsHtml = dataObj
@@ -87,14 +88,22 @@ const server = http.createServer((req, res) => {
             /{%PRODUCT_CARDS%}/g,
             cardsHtml
         );
-        res.end(output)
+
+        res.end(output);
     }
     // Product page
-    else if (pathName === "/product") {
-        res.end("This is the PRODUCT");
+    else if (pathname === "/product") {
+        res.writeHead(200, {
+            "Content-Type": "text/html",
+        });
+
+        const product = dataObj[query.id];
+        const output = replaceTemplate(templateProduct, product);
+
+        res.end(output);
     }
     // API
-    else if (pathName === "/api") {
+    else if (pathname === "/api") {
         res.writeHead(200, {
             "Content-Type": "application/json",
         });
