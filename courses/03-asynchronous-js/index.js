d9c6a257e6d8e0dee636bca28d59bcc02c46d730
magnificodev <magnificodev8@gmail.com>
2024-08-20 00:26:24 +0700

    Solve Callback Hell Problem using Promise

--- courses/03-asynchronous-js/index.js
+++ courses/03-asynchronous-js/index.js
@@ -1,21 +1,46 @@
 const fs = require("fs");
-
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
-    console.log(`Breed: ${data}`);
-    superagent
-        .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .end((err, res) => {
-            if (err) return console.log(err.message);
-
-            fs.appendFile(
-                `${__dirname}/image-links.txt`,
-                `${res.body.message}\n`,
-                (err) => {
-                    if (err) return console.log(err.message);
-                    console.log("Written successfully!");
-                }
-            );
+const readFilePro = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, "utf-8", (err, data) => {
+            if (err) reject(err);
+            resolve(data);
         });
-});
+    });
+};
+
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) reject(err.message);
+            resolve("Written Successfully!");
+        });
+    });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+    .then((data) => superagent.get(`https://dog.ceo/api/breed/${data}/images/random`))
+    .then(res => writeFilePro("./image-links.txt", res.body.message))
+    .then(data => console.log(data))
+    .catch((err) => console.log(err.message));
+
+// Normal way
+// fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
+//     console.log(`Breed: ${data}`);
+//     superagent
+//         .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//         .then((res) => {
+//             fs.appendFile(
+//                 `${__dirname}/image-links.txt`,
+//                 `${res.body.message}\n`,
+//                 (err) => {
+//                     if (err) return console.log(err.message);
+//                     console.log("Written successfully!");
+//                 }
+//             );
+//         })
+//         .catch((err) => {
+//             console.log(err.message);
+//         });
+// });
