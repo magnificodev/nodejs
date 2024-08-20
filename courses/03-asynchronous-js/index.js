791ccec0f6cf0559bf4edc8fb530e855e7c90f43
magnificodev <magnificodev8@gmail.com>
2024-08-20 19:44:48 +0700

    Waiting for Multiple Promises Simultaneously

--- courses/03-asynchronous-js/index.js
+++ courses/03-asynchronous-js/index.js
@@ -22,25 +22,40 @@ const writeFilePro = (file, data) => {
 const getDogPic = async () => {
     try {
         const data = await readFilePro(`${__dirname}/dog.txt`);
-        const res = await superagent.get(
+
+        const resPro1 = superagent.get(
             `https://dog.ceo/api/breed/${data}/images/random`
         );
-        await writeFilePro("./image-links.txt", res.body.message);
+        const resPro2 = superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+        const resPro3 = superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+
+        const all = await Promise.all([resPro1, resPro2, resPro3])
+        const imgs = all.map(res => res.body.message)
+        console.log(imgs)
+
+        await writeFilePro("./image-links.txt", imgs.join("\n"));
     } catch (err) {
-        throw(err)
+        throw err;
     }
     return "Hello from getDogPic() function";
 };
+getDogPic()
 
-(async () => {
-    try {
-        const res = await getDogPic();
-        console.log(res)
-    }
-    catch (err) {
-        console.log(err)
-    }
-})()
+
+// Using IIFE
+// (async () => {
+//     try {
+//         const res = await getDogPic();
+//         console.log(res);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// })();
+
 
 
 // Using async/await
@@ -50,15 +65,12 @@ const getDogPic = async () => {
 
 // console.log("Hello from Magnificodev outside of the box");
 
-
 // Using promises
 // readFilePro(`${__dirname}/dog.txt`)
 //     .then((data) => superagent.get(`https://dog.ceo/api/breed/${data}/images/random`))
 //     .then(res => writeFilePro("./image-links.txt", res.body.message))
 //     .then(data => console.log(data))
 //     .catch((err) => console.log(err.message));
-
-
 
 // Normal way
 // fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
