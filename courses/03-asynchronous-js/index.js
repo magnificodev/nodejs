41db5ce86d3a9217a242c5607661092ca209f91c
magnificodev <magnificodev8@gmail.com>
2024-08-20 19:04:32 +0700

    Comsuming Promises with Async/Await

--- courses/03-asynchronous-js/index.js
+++ courses/03-asynchronous-js/index.js
@@ -19,11 +19,23 @@ const writeFilePro = (file, data) => {
     });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-    .then((data) => superagent.get(`https://dog.ceo/api/breed/${data}/images/random`))
-    .then(res => writeFilePro("./image-links.txt", res.body.message))
-    .then(data => console.log(data))
-    .catch((err) => console.log(err.message));
+const getDogPic = async () => {
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`);
+        const res = await superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+        await writeFilePro("./image-links.txt", res.body.message)
+    } catch (err) {
+        console.log(err);
+    }
+};
+getDogPic()
+// readFilePro(`${__dirname}/dog.txt`)
+//     .then((data) => superagent.get(`https://dog.ceo/api/breed/${data}/images/random`))
+//     .then(res => writeFilePro("./image-links.txt", res.body.message))
+//     .then(data => console.log(data))
+//     .catch((err) => console.log(err.message));
 
 // Normal way
 // fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
